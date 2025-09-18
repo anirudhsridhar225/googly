96f139e0c9745918dd48bab02d03e5df1557539d
anirudhsridhar225 <anirudhsridhar1064@gmail.com>
2025-09-19 01:27:56 +0530

    [feat]: wonky loading screen, needs to be fixed

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Loader from "@/components/Loader";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Global loader overlay */}
+        <Loader />
         {children}
       </body>
     </html>
